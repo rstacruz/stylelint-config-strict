9a865a216e5964bebf6443fea086b1e21b0a8022
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2016-09-20 06:34:31 +0800

    Release v13.0.1

--- index.js
+++ index.js
@@ -3,11 +3,17 @@ module.exports = {
     require.resolve('stylelint-config-standard')
   ],
   rules: {
+    'at-rule-no-vendor-prefix': true,
+    'function-url-no-scheme-relative': true, // use https!
     'function-url-quotes': "always",
+    'max-nesting-depth': 3,
+    'media-feature-name-no-vendor-prefix': true,
+    'property-no-vendor-prefix': true,
     'selector-attribute-quotes': "always",
     'selector-no-id': true,
     'selector-no-vendor-prefix': true,
     'string-quotes': 'single',
-    'value-keyword-case': "lower",
+    'value-keyword-case': 'lower',
+    'value-no-vendor-prefix': true,
   }
 }
